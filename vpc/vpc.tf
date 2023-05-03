9ac13e56924bc13576c8a0fa4fa380862a90cda7
Ankarao <veeranki2014@gmail.com>
2023-05-03 21:30:00 +0530

    somemsg

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -32,3 +32,15 @@ resource "aws_internet_gateway" "automated_IGW" {
   
 }
 
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.automated_IGW.id
+    }
+    tags {
+        Name = "public_rt"
+    }
+  
+}
+
