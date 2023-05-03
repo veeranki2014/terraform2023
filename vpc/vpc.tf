310632986d1b1648caa6d2ec1c89efb8d283e692
Ankarao <veeranki2014@gmail.com>
2023-05-03 21:56:15 +0530

    somemsg

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -44,13 +44,17 @@ resource "aws_route_table" "public_rt" {
   
 }
 
-# resource "aws_route_table" "private_rit" {
-#     vpc_id = aws_vpc.main.id
-#     route{
-#         cidr_block = ""
-#     }
+resource "aws_route_table" "private_rt" {
+    vpc_id = aws_vpc.main.id
+    route{
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.automated_IGW.id
+    }
+    tags = {
+        Name = "private-rt"
+    }
   
-# }
+}
 
 resource "aws_eip" "auto_eip" {    
     tags = {
