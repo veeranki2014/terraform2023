dd6addd3badddc507ab6c3c1b095a6b2a0cb6766
Ankarao <veeranki2014@gmail.com>
2023-05-03 22:03:59 +0530

    somemsg

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -79,3 +79,8 @@ resource "aws_route_table_association" "public" {
     route_table_id = aws_route_table.public_rt.id
   
 }
+
+resource "aws_route_table_association" "private" {
+    subnet_id = aws_subnet.private.id
+    route_table_id = aws_route_table.private_rt.id
+}
