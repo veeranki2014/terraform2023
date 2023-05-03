dfb1475659cf7a4a11058c2c0f280939e12d435a
Ankarao <veeranki2014@gmail.com>
2023-05-03 21:23:21 +0530

    somemsg

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
     vpc_id = aws_vpc.main.id
-    cidr_block = "10.2.0.0/24"
+    cidr_block = "10.0.2.0/24"
     tags = {
       Name = "private_subnet_automated_subnet"
     }
