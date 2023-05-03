07820adff0d193bec123153d3318c1d16aa54477
Ankarao <veeranki2014@gmail.com>
2023-05-03 21:13:34 +0530

    somemsg

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "main" {
     }  
 }
 
-resource "aws_subnet" "subnet_auto_" {
+resource "aws_subnet" "public" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.1.0/24"
     tags = {
@@ -14,7 +14,7 @@ resource "aws_subnet" "subnet_auto_" {
     }  
 }
 
-resource "aws_subnet" "subnet_auto" {
+resource "aws_subnet" "private" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.2.0/24"
     tags = {
