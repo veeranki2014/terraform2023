1f1fd21a92d5036b62b8b44bc653c8f1554d7eec
Ankarao <veeranki2014@gmail.com>
2023-05-03 21:12:01 +0530

    somemsg

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -6,11 +6,20 @@ resource "aws_vpc" "main" {
     }  
 }
 
-resource "aws_subnet" "subnet_auto" {
+resource "aws_subnet" "subnet_auto_" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.1.0/24"
     tags = {
       Name = "Public_Subnet_automated_subnet"
     }  
+}
+
+resource "aws_subnet" "subnet_auto" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.2.0/24"
+    tags = {
+      Name = "private_subnet_automated_subnet"
+    }
+  
 }
 
