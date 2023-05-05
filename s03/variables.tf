54d30a9367eef44954d08571a1a63b4ee69dcd2d
Ankarao <veeranki2014@gmail.com>
2023-05-05 16:50:36 +0530

    somemsg

--- s03/variables.tf
+++ s03/variables.tf
@@ -2,3 +2,6 @@ variable "cidr" {
     type = string  
     default = "10.0.0.0/24" ## if don't define this default terraform ask us at runtime.
 }
+variable "public_subnet_cidr" {
+    type = string  
+}
